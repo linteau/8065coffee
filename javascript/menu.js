2e79d8ac1e15644a88bc627529b2f332ea558853
andylinteau <andy.linteau@buystand.com>
2013-01-11 05:37:34 +0800

    add java interaction on menu page

---
+++ javascript/menu.js
@@ -0,0 +1,36 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1187\cocoasubrtf340
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+
+\f0\fs24 \cf0  <script>\
+        function myFunction()\
+        \{\
+            document.getElementById("menuHeading").innerHTML="Bread";\
+            document.getElementById("menuContent").innerHTML="Cinnamon Bread .........................40<br>Toasted Bread w/ Butter ..............30<br>Toasted Bread w/ Hazelnut ...........40<br>Grilled Cheese ..........................60<br>Toasted Bread w/ Butter & Bacon ...80<br>Pastillas per Piece .......................5<br>Pastillas per Pack .......................50";\
+            document.getElementById("rightMenuHeading").style.display="none";\
+            document.getElementById("rightMenuContent").style.display="none";\
+			document.getElementById("foodSubNav").style.color="white";\
+			document.getElementById("drinkSubNav").style.color="black";\
+\
+            \
+        \}\
+        function drinkFunction()\
+        \{\
+            document.getElementById("menuHeading").innerHTML="Hot Drinks";\
+            document.getElementById("menuContent").innerHTML="Caffe Latte'.......60<br>Cappuccino .......60<br>Brewed Coffee ...30<br>Americano ........40<br>Caffee Mocha ....80</div>";\
+            document.getElementById("rightMenuHeading").style.display="block";\
+            document.getElementById("rightMenuContent").style.display="block";\
+			document.getElementById("foodSubNav").style.color="black";\
+			document.getElementById("drinkSubNav").style.color="white";		\
+        \}\
+		function leftSubMenuMouseOver()\{		\
+			document.getElementById("leftSubMenuBox").style.backgroundColor="#fc0";\
+			\}\
+		function leftSubMenuMouseOut()\{		\
+			document.getElementById("leftSubMenuBox").style.backgroundColor="#FC9";\
+			\}\
+		\
+		\
+    </script>}
